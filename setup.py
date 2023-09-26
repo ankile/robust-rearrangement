6d070b24c43ba8ea7662a75a9fa3f7c927043ef8
ankile <lars.ankile@gmail.com>
2023-09-26 11:29:17 -0400

    Restructure the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building furniture with diffusion',
+    author='ankile',
+    license='MIT',
+)
