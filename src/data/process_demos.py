345405118c9f3e9d8a239075128aaf070fd4e07a
ankile <lars.ankile@gmail.com>
2023-10-06 09:00:36 -0400

    Better batch proc in data proc

--- src/data/process_demos.py
+++ src/data/process_demos.py
@@ -15,68 +15,129 @@ from ipdb import set_trace as bp
 device = torch.device("cuda:1")
 
 
+# def process_demos_to_feature(input_path, output_path, encoder, batch_size=256):
+#     file_paths = glob(f"{input_path}/**/*.pkl", recursive=True)
+#     n_state_action_pairs = len(file_paths)
+
+#     print(f"Number of trajectories: {n_state_action_pairs}")
+
+#     observations = []
+#     actions = []
+#     episode_ends = []
+
+#     end_index = 0
+#     for path in tqdm(file_paths):
+#         with open(path, "rb") as f:
+#             data = pickle.load(f)
+
+#         actions += data["actions"]
+#         end_index += len(data["actions"])
+#         obs = data["observations"]
+#         robot_state = np.stack(
+#             [filter_and_concat_robot_state(o["robot_state"]) for o in obs]
+#         )
+
+#         imgs1 = torch.from_numpy(np.stack([o["color_image1"] for o in obs])).to(device)
+#         imgs2 = torch.from_numpy(np.stack([o["color_image2"] for o in obs])).to(device)
+
+#         def process_in_batches(tensor, encoder):
+#             n = len(tensor)
+#             features_list = []
+
+#             for i in range(0, n, batch_size):
+#                 batch = tensor[i : i + batch_size]
+#                 features = encoder(batch)
+#                 features_list.append(features)
+
+#             return torch.cat(features_list, dim=0)
+
+#         features1 = process_in_batches(imgs1, encoder)
+#         features2 = process_in_batches(imgs2, encoder)
+
+#         # Concatenate the robot state and the image features
+#         obs = np.concatenate(
+#             [robot_state, features1.cpu().numpy(), features2.cpu().numpy()], axis=-1
+#         )
+
+#         # Extend the observations list with the current trajectory.
+#         observations += obs.tolist()
+
+#         # Add the end index to the overall list.
+#         episode_ends.append(end_index)
+
+#     # Convert the lists to numpy arrays.
+#     observations = np.array(observations)
+#     actions = np.array(actions)
+#     episode_ends = np.array(episode_ends)
+
+#     # Save the data to a zarr file.
+#     zarr.save(
+#         f"{output_path}/data.zarr",
+#         observations=observations,
+#         actions=actions,
+#         episode_ends=episode_ends,
+#     )
+
+
 def process_demos_to_feature(input_path, output_path, encoder, batch_size=256):
     file_paths = glob(f"{input_path}/**/*.pkl", recursive=True)
-    n_state_action_pairs = len(file_paths)
+    print(f"Number of trajectories: {len(file_paths)}")
 
-    print(f"Number of trajectories: {n_state_action_pairs}")
-
-    observations = []
-    actions = []
-    episode_ends = []
-
+    observations, actions, episode_ends = [], [], []
+    img_buffer1, img_buffer2 = [], []
+    features_list1, features_list2 = [], []
     end_index = 0
+
+    def process_buffer(buffer, encoder):
+        tensor = torch.stack(buffer).to(device)
+        features = encoder(tensor)
+        return features.cpu().numpy()
+
     for path in tqdm(file_paths):
         with open(path, "rb") as f:
             data = pickle.load(f)
 
         actions += data["actions"]
         end_index += len(data["actions"])
+
         obs = data["observations"]
         robot_state = np.stack(
             [filter_and_concat_robot_state(o["robot_state"]) for o in obs]
         )
 
-        imgs1 = torch.from_numpy(np.stack([o["color_image1"] for o in obs])).to(device)
-        imgs2 = torch.from_numpy(np.stack([o["color_image2"] for o in obs])).to(device)
+        img_buffer1 += [torch.from_numpy(o["color_image1"]) for o in obs]
+        img_buffer2 += [torch.from_numpy(o["color_image2"]) for o in obs]
 
-        def process_in_batches(tensor, encoder):
-            n = len(tensor)
-            features_list = []
+        while len(img_buffer1) >= batch_size:
+            features1 = process_buffer(img_buffer1[:batch_size], encoder)
+            features2 = process_buffer(img_buffer2[:batch_size], encoder)
+            features_list1.append(features1)
+            features_list2.append(features2)
+            img_buffer1, img_buffer2 = (
+                img_buffer1[batch_size:],
+                img_buffer2[batch_size:],
+            )
 
-            for i in range(0, n, batch_size):
-                batch = tensor[i : i + batch_size]
-                features = encoder(batch)
-                features_list.append(features)
-
-            return torch.cat(features_list, dim=0)
-
-        features1 = process_in_batches(imgs1, encoder)
-        features2 = process_in_batches(imgs2, encoder)
-
-        # Concatenate the robot state and the image features
-        obs = np.concatenate(
-            [robot_state, features1.cpu().numpy(), features2.cpu().numpy()], axis=-1
+        # Concatenate the robot state and image features
+        features1, features2 = np.concatenate(features_list1, axis=0), np.concatenate(
+            features_list2, axis=0
         )
+        obs = np.concatenate([robot_state, features1, features2], axis=-1)
 
-        # Extend the observations list with the current trajectory.
         observations += obs.tolist()
-
-        # Add the end index to the overall list.
         episode_ends.append(end_index)
 
-    # Convert the lists to numpy arrays.
+    # Don't forget to process any remaining items in the buffer
+    if len(img_buffer1) > 0:
+        features1 = process_buffer(img_buffer1, encoder)
+        features2 = process_buffer(img_buffer2, encoder)
+        features_list1.append(features1)
+        features_list2.append(features2)
+
+    # Convert to numpy arrays
     observations = np.array(observations)
     actions = np.array(actions)
     episode_ends = np.array(episode_ends)
-
-    # Save the data to a zarr file.
-    zarr.save(
-        f"{output_path}/data.zarr",
-        observations=observations,
-        actions=actions,
-        episode_ends=episode_ends,
-    )
 
 
 if __name__ == "__main__":
