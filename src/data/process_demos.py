fdadba4013d63c5239affb6390e7eb1472e7afc4
ankile <lars.ankile@gmail.com>
2023-10-06 09:46:08 -0400

    Fix many bugs with proc code

--- src/data/process_demos.py
+++ src/data/process_demos.py
@@ -12,80 +12,14 @@ from furniture_bench.robot.robot_state import filter_and_concat_robot_state
 
 from ipdb import set_trace as bp
 
-device = torch.device("cuda:1")
-
-
-# def process_demos_to_feature(input_path, output_path, encoder, batch_size=256):
-#     file_paths = glob(f"{input_path}/**/*.pkl", recursive=True)
-#     n_state_action_pairs = len(file_paths)
-
-#     print(f"Number of trajectories: {n_state_action_pairs}")
-
-#     observations = []
-#     actions = []
-#     episode_ends = []
-
-#     end_index = 0
-#     for path in tqdm(file_paths):
-#         with open(path, "rb") as f:
-#             data = pickle.load(f)
-
-#         actions += data["actions"]
-#         end_index += len(data["actions"])
-#         obs = data["observations"]
-#         robot_state = np.stack(
-#             [filter_and_concat_robot_state(o["robot_state"]) for o in obs]
-#         )
-
-#         imgs1 = torch.from_numpy(np.stack([o["color_image1"] for o in obs])).to(device)
-#         imgs2 = torch.from_numpy(np.stack([o["color_image2"] for o in obs])).to(device)
-
-#         def process_in_batches(tensor, encoder):
-#             n = len(tensor)
-#             features_list = []
-
-#             for i in range(0, n, batch_size):
-#                 batch = tensor[i : i + batch_size]
-#                 features = encoder(batch)
-#                 features_list.append(features)
-
-#             return torch.cat(features_list, dim=0)
-
-#         features1 = process_in_batches(imgs1, encoder)
-#         features2 = process_in_batches(imgs2, encoder)
-
-#         # Concatenate the robot state and the image features
-#         obs = np.concatenate(
-#             [robot_state, features1.cpu().numpy(), features2.cpu().numpy()], axis=-1
-#         )
-
-#         # Extend the observations list with the current trajectory.
-#         observations += obs.tolist()
-
-#         # Add the end index to the overall list.
-#         episode_ends.append(end_index)
-
-#     # Convert the lists to numpy arrays.
-#     observations = np.array(observations)
-#     actions = np.array(actions)
-#     episode_ends = np.array(episode_ends)
-
-#     # Save the data to a zarr file.
-#     zarr.save(
-#         f"{output_path}/data.zarr",
-#         observations=observations,
-#         actions=actions,
-#         episode_ends=episode_ends,
-#     )
-
 
 def process_demos_to_feature(input_path, output_path, encoder, batch_size=256):
     file_paths = glob(f"{input_path}/**/*.pkl", recursive=True)
     print(f"Number of trajectories: {len(file_paths)}")
 
     observations, actions, episode_ends = [], [], []
-    img_buffer1, img_buffer2 = [], []
-    features_list1, features_list2 = [], []
+    robot_state_buffer, img_buffer1, img_buffer2 = [], [], []
+    robot_state_list, features_list1, features_list2 = [], [], []
     end_index = 0
 
     def process_buffer(buffer, encoder):
@@ -101,19 +35,21 @@ def process_demos_to_feature(input_path, output_path, encoder, batch_size=256):
         end_index += len(data["actions"])
 
         obs = data["observations"]
-        robot_state = np.stack(
-            [filter_and_concat_robot_state(o["robot_state"]) for o in obs]
-        )
 
+        robot_state_buffer += [
+            filter_and_concat_robot_state(o["robot_state"]) for o in obs
+        ]
         img_buffer1 += [torch.from_numpy(o["color_image1"]) for o in obs]
         img_buffer2 += [torch.from_numpy(o["color_image2"]) for o in obs]
 
         while len(img_buffer1) >= batch_size:
             features1 = process_buffer(img_buffer1[:batch_size], encoder)
             features2 = process_buffer(img_buffer2[:batch_size], encoder)
+            robot_state_list.append(np.stack(robot_state_buffer[:batch_size]))
             features_list1.append(features1)
             features_list2.append(features2)
-            img_buffer1, img_buffer2 = (
+            robot_state_buffer, img_buffer1, img_buffer2 = (
+                robot_state_buffer[batch_size:],
                 img_buffer1[batch_size:],
                 img_buffer2[batch_size:],
             )
@@ -127,13 +63,15 @@ def process_demos_to_feature(input_path, output_path, encoder, batch_size=256):
         )
         if features1.size == 0 or features2.size == 0:
             continue  # Skip the current file as it has no valid features
-        obs = np.concatenate([robot_state, features1, features2], axis=-1)
+
+        robot_state_array = np.concatenate(robot_state_list, axis=0)
+        obs = np.concatenate([robot_state_array, features1, features2], axis=-1)
 
         observations += obs.tolist()
         episode_ends.append(end_index)
 
         # Reset the features_list for the next iteration
-        features_list1, features_list2 = [], []
+        robot_state_list, features_list1, features_list2 = [], [], []
 
     # Don't forget to process any remaining items in the buffer
     if len(img_buffer1) > 0:
@@ -156,8 +94,12 @@ if __name__ == "__main__":
     parser.add_argument("--encoder", "-c", type=str)
     parser.add_argument("--furniture", "-f", type=str)
     parser.add_argument("--batch-size", "-b", type=int, default=256)
+    parser.add_argument("--gpu-id", "-g", type=int, default=0)
 
     args = parser.parse_args()
+
+    global device
+    device = torch.device(f"cuda:{args.gpu_id}")
 
     if args.obs_out == "feature":
         assert args.encoder is not None, "Must specify encoder when using feature obs"
--- src/models/vision.py
+++ src/models/vision.py
@@ -1,15 +1,23 @@
 import torch
 import transformers
 from vip import load_vip
+from r3m import load_r3m
+from ipdb import set_trace as bp
 
 
 def get_encoder(encoder_name, freeze=True, device="cuda"):
     if encoder_name == "dinov2-base":
         return DinoEncoder(size="base", freeze=freeze, device=device)
-    elif encoder_name == "vip":
-        return transformers.AutoModel.from_pretrained(
-            "google/vit-large-patch16-224-in21k"
-        ).to(device)
+    if encoder_name == "dinov2-large":
+        return DinoEncoder(size="large", freeze=freeze, device=device)
+    if encoder_name == "dinov2-small":
+        return DinoEncoder(size="small", freeze=freeze, device=device)
+    # elif encoder_name == "vip":
+    #     return transformers.AutoModel.from_pretrained(
+    #         "google/vit-large-patch16-224-in21k"
+    #     ).to(device)
+    elif encoder_name == "r3m":
+        return
     else:
         raise ValueError(f"Unknown encoder name: {encoder_name}")
 
@@ -44,6 +52,29 @@ class VIPEncoder(torch.nn.Module):
         super().__init__()
         self.device = device
         self.model = load_vip(device=device).module
+        self.encoding_dim = self.model.convnet.fc.out_features
+
+        if freeze:
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+            self.model.eval()
+
+    # Expect input to be a batch of images of shape (batch_size, 224, 224, 3) in range [0, 255]
+    def forward(self, x):
+        # Move channels to the front
+        x = x.permute(0, 3, 1, 2)
+        x = self.model(x)
+        return x
+
+
+class R3MEncoder(torch.nn.Module):
+    def __init__(self, size=50, freeze=True, device="cuda", *args, **kwargs) -> None:
+        super().__init__()
+        assert size in [18, 34, 50]
+
+        self.device = device
+        self.model = load_r3m(modelid=f"resnet{size}", device=device).module
         self.encoding_dim = self.model.convnet.fc.out_features
 
         if freeze:
