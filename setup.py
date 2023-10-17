b6f40b2f8b0ca176fe2de0933d7f69d86a6dd044
ankile <lars.ankile@gmail.com>
2023-10-17 10:34:17 -0400

    Add reqs to the setup fule

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Building furniture with diffusion',
-    author='ankile',
-    license='MIT',
+    version="0.1.0",
+    description="Building furniture with diffusion",
+    author="ankile",
+    license="MIT",
+    install_requires=required,
 )
