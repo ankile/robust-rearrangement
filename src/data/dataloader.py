0d4fa87fda70339ddbab9576ecb9e596e48bf7fa
ankile <lars.ankile@gmail.com>
2023-10-21 12:20:18 -0400

    Clean notebooks folder

---
+++ src/data/dataloader.py
@@ -0,0 +1,13 @@
+import torch
+import itertools
+
+
+class FixedStepsDataloader(torch.utils.data.DataLoader):
+    def __init__(self, *args, n_batches, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_batches = n_batches
+
+    def __iter__(self):
+        endless_dataloader = itertools.cycle(super().__iter__())
+        for _ in range(self.n_batches):
+            yield next(endless_dataloader)
