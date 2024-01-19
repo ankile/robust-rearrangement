97fea5802e95970f4451c9bf0900d1ca7a095440
ankile <lars.ankile@gmail.com>
2024-01-19 14:54:43 -0500

    Add script for easily resume runs

---
+++ src/train/continue_bc.py
@@ -0,0 +1,31 @@
+import argparse
+from ml_collections import ConfigDict
+
+from wandb import Api
+
+
+from src.train.train_bc import main as train_bc_main
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--run-id", type=str, required=True)
+    args = parser.parse_args()
+
+    # Get the model file and config
+    api = Api()
+    run = api.run(f"robot-rearrangement/{args.run_id}")
+    model_file = [f for f in run.files() if f.name.endswith(".pt")][0]
+    model_path = model_file.download(exist_ok=True).name
+    config = ConfigDict(run.config)
+
+    # Get the epoch the model was saved at
+    start_epoch = run.summary["epoch"]
+
+    # Add the new data to the config
+    config.load_checkpoint_path = model_path
+
+    # Change any parameters here
+    config.num_epochs = 200
+
+    train_bc_main(config, start_epoch=start_epoch)
