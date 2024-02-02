1a058b0722804360bfda557af2bb6c16303943c8
ankile <lars.ankile@gmail.com>
2024-02-02 11:57:46 -0500

    Implement gaussian normalizer and calculate data stats across the dataset

--- src/dataset/normalizer.py
+++ src/dataset/normalizer.py
@@ -2,46 +2,62 @@ from typing import Union
 import torch
 import torch.nn as nn
 import numpy as np
-from src.dataset.data_stats import data_stats
+
+from src.dataset.data_stats import get_data_stats
 
 
-def get_data_stats(data):
-    data = data.reshape(-1, data.shape[-1])
-    stats = {"min": np.min(data, axis=0), "max": np.max(data, axis=0)}
-    return stats
+def create_parameter_dict(stats, action_key, normalization_mode):
+    param_dict = nn.ParameterDict()
+    for key, value in stats.items():
+        # Skip the action type that is not chosen
+        if key in ["action/pos", "action/delta"] and key != action_key:
+            continue
+
+        # Rename the chosen action key
+        if key == action_key:
+            key = "action"
+
+        if isinstance(value, dict) and (
+            ("min" in value and "max" in value) or ("mean" in value and "std" in value)
+        ):
+            if normalization_mode == "min_max":
+                param_dict[key] = nn.ParameterDict(
+                    {
+                        "min": nn.Parameter(torch.tensor(value["min"])),
+                        "max": nn.Parameter(torch.tensor(value["max"])),
+                    }
+                )
+            elif normalization_mode == "mean_std":
+                param_dict[key] = nn.ParameterDict(
+                    {
+                        "mean": nn.Parameter(torch.tensor(value["mean"])),
+                        "std": nn.Parameter(torch.tensor(value["std"])),
+                    }
+                )
+            else:
+                raise ValueError(
+                    f"Normalization mode {normalization_mode} not recognized."
+                )
+        elif isinstance(value, dict):
+            param_dict[key] = create_parameter_dict(
+                value, action_key, normalization_mode
+            )
+        else:
+            raise ValueError(f"Value {value} of type {type(value)} not recognized.")
+    return param_dict
 
 
-# For now, these stats come from calculating the min/max of data
-# collected from the robot in the sim for the `one_leg` task,
-# for ~200 episodes after the change in the controller
-class StateActionNormalizer(nn.Module):
+class LinearNormalizer(nn.Module):
     def __init__(self, control_mode="delta"):
         super().__init__()
         assert control_mode in ["delta", "pos"]
 
-        self.stats = nn.ParameterDict(
-            {
-                "robot_state": nn.ParameterDict(
-                    {
-                        "min": nn.Parameter(
-                            torch.tensor(data_stats["robot_state"]["min"])
-                        ),
-                        "max": nn.Parameter(
-                            torch.tensor(data_stats["robot_state"]["max"])
-                        ),
-                    }
-                ),
-                "action": nn.ParameterDict(
-                    {
-                        "min": nn.Parameter(
-                            torch.tensor(data_stats[f"action/{control_mode}"]["min"])
-                        ),
-                        "max": nn.Parameter(
-                            torch.tensor(data_stats[f"action/{control_mode}"]["max"])
-                        ),
-                    }
-                ),
-            }
+        stats = get_data_stats()
+
+        self.stats = create_parameter_dict(
+            stats=stats,
+            action_key=f"action/{control_mode}",
+            normalization_mode="min_max",
         )
 
         # Turn off gradients for the stats
@@ -100,3 +116,88 @@ class StateActionNormalizer(nn.Module):
                 stats[key][stat] = self.stats[key][stat].cpu().numpy()
 
         return stats
+
+
+class GaussianNormalizer(nn.Module):
+    def __init__(self, control_mode="delta"):
+        super().__init__()
+        assert control_mode in ["delta", "pos"]
+
+        stats = get_data_stats()
+
+        self.stats = create_parameter_dict(
+            stats=stats,
+            action_key=f"action/{control_mode}",
+            normalization_mode="mean_std",
+        )
+
+        # Turn off gradients for the stats
+        for key in self.stats.keys():
+            for stat in self.stats[key].keys():
+                self.stats[key][stat].requires_grad = False
+
+    def _normalize(self, x, key):
+        stats = self.stats[key]
+        x = (x - stats["mean"]) / stats["std"]
+        return x
+
+    def _denormalize(self, x, key):
+        stats = self.stats[key]
+        x = x * stats["std"] + stats["mean"]
+        return x
+
+    def forward(
+        self,
+        x: Union[np.ndarray, torch.Tensor],
+        key: str,
+        forward: bool = True,
+    ) -> Union[np.ndarray, torch.Tensor]:
+        """
+        Normalize or denormalize the input data.
+
+        It accepts either a numpy array or a torch tensor and will return the same type.
+        """
+        numpy = False
+        if isinstance(x, np.ndarray):
+            x = torch.from_numpy(x).float()
+            numpy = True
+
+        if forward:
+            x = self._normalize(x, key)
+        else:
+            x = self._denormalize(x, key)
+
+        if numpy:
+            return x.numpy()
+        return x
+
+    def keys(self):
+        return self.stats.keys()
+
+    @property
+    def stats_dict(self):
+        # Return the stats as a dict of numpy arrays
+        stats = {}
+
+        for key in self.stats.keys():
+            stats[key] = {}
+            for stat in self.stats[key].keys():
+                stats[key][stat] = self.stats[key][stat].cpu().numpy()
+
+        return stats
+
+
+if __name__ == "__main__":
+    # Test the normalizers
+    import matplotlib.pyplot as plt
+
+    # Create the normalizers
+    linear_normalizer = LinearNormalizer()
+    gaussian_normalizer = GaussianNormalizer()
+
+    # Print the stats
+    print("Linear normalizer stats:")
+    print(linear_normalizer.stats_dict)
+
+    print("Gaussian normalizer stats:")
+    print(gaussian_normalizer.stats_dict)
