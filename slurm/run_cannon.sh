276c24f1e719f49d1244695c5c746be3e930cbe5
ankile <lars.ankile@gmail.com>
2024-02-21 17:15:16 -0500

    Add the slurm config

--- slurm/run_cannon.sh
+++ slurm/run_cannon.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #SBATCH -p gpu                
-#SBATCH -t 0-16:00            
-#SBATCH --mem=256G            
+#SBATCH -t 0-15:00            
+#SBATCH --mem=128G            
 #SBATCH --gres=gpu:1          
 #SBATCH -c 32                 
 #SBATCH -o wandb_output_%j.log  
 #SBATCH -e wandb_error_%j.log   
 
-
-
 # Run the wandb agent command
 # python -m src.train.bc_no_rollout +experiment=image_multitask
 # python -m src.train.bc_no_rollout +experiment=image_traj_aug furniture=square_table
 # python -m src.train.bc_no_rollout +experiment=image_baseline furniture=square_table
-python -m src.train.bc_no_rollout +experiment=image_collect_infer furniture=square_table
+# python -m src.train.bc_no_rollout +experiment=image_collect_infer furniture=square_table
+python -m src.train.bc_no_rollout +experiment=image_cherry_noise furniture=round_table
