aa5ac842b7e008446bd753cb5af7de87b4a25a42
ankile <lars.ankile@gmail.com>
2024-02-20 22:50:49 -0500

    Make supercloud wandb offline

--- slurm/run_sweep_supercloud.sh
+++ slurm/run_sweep_supercloud.sh
@@ -13,4 +13,4 @@
 # Run the wandb agent command
 # wandb agent robot-rearrangement/sweeps/44hsqeuy
 # python -m src.train.bc +experiment=image_training vision_encoder=resnet rollout=rollout wandb.project=baselines-test wandb.name=50-lamp-teleop-resnet-40 furniture=lamp randomness=low
-python -m src.train.bc_no_rollout +experiment=image_multitask
+python -m src.train.bc_no_rollout +experiment=image_multitask wandb.mode=offine
