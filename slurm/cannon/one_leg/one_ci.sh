88eba7fecb99dd2635a9520804416c42ea5b938f
ankile <lars.ankile@gmail.com>
2024-02-25 15:40:18 -0500

    More tweaks and jobs going

--- slurm/cannon/one_leg/one_ci.sh
+++ slurm/cannon/one_leg/one_ci.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH -p gpu                
-#SBATCH -t 0-08:00            
+#SBATCH -t 1-16:00            
 #SBATCH --mem=128G            
 #SBATCH --gres=gpu:1          
-#SBATCH -c 32                 
+#SBATCH -c 16              
 #SBATCH -o wandb_output_%j.log  
 #SBATCH -e wandb_error_%j.log   
 
-python -m src.train.bc_no_rollout +experiment=image_collect_infer furniture=one_leg
+python -m src.train.bc_no_rollout +experiment=image_collect_infer furniture=one_leg data.dataloader_workers=16
---
+++ slurm/cannon/square_table/sq_bl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -p gpu
+#SBATCH -t 1-8:00
+#SBATCH --mem=250G
+#SBATCH --gres=gpu:1
+#SBATCH -c 16
+#SBATCH -o wandb_output_%j.log
+#SBATCH -e wandb_error_%j.log
+
+python -m src.train.bc_no_rollout +experiment=image_baseline furniture=square_table data.dataloader_workers=16
