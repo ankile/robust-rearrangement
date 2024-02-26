b7482c7321da5203d57815711c9f67cd2a1ff625
ankile <lars.ankile@gmail.com>
2024-02-26 10:24:12 -0500

    Add sweep slurm script and organize slurm files

--- slurm/cannon/sweeps/one_ta_sw_1.sh
+++ slurm/cannon/sweeps/one_ta_sw_1.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #SBATCH -p gpu                
-#SBATCH -t 0-16:00            
-#SBATCH --mem=256G            
+#SBATCH -t 3-00:00            
+#SBATCH --mem=240G            
 #SBATCH --gres=gpu:1          
-#SBATCH -c 32                 
+#SBATCH -c 16
 #SBATCH -o wandb_output_%j.log  
 #SBATCH -e wandb_error_%j.log   
-
-
 
 # Run the wandb agent command
 wandb agent robot-rearrangement/sweeps/44hsqeuy # Square table baseline
