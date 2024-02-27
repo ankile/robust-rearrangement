330123da372a5e637cf7ece7b9a796043168d2f7
ankile <lars.ankile@gmail.com>
2024-02-27 11:37:47 -0500

    Scripts for running comparison with untrained resnet

---
+++ slurm/cannon/one_leg/one_bl_npt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -p gpu
+#SBATCH -t 0-16:00
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:1
+#SBATCH -c 16
+#SBATCH -o wandb_output_%j.log
+#SBATCH -e wandb_error_%j.log
+
+python -m src.train.bc_no_rollout \
+    +experiment=image_baseline \
+    furniture=one_leg \
+    data.dataloader_workers=16 \
+    vision_encoder.pretrained=false \
+    wandb.name=one_bl_no_pretrain
