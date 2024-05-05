ba399d41e12ad864cea4a76643644a8422a73908
ankile <lars.ankile@gmail.com>
2024-05-05 16:19:53 -0400

    Relaunch real world experiments that failed bc slurm

--- slurm/bc/improbable/one_leg_insert/real_one_leg_insert_vib.sh
+++ slurm/bc/improbable/one_leg_insert/real_one_leg_insert_vib.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -p vision-pulkitag-h100,vision-pulkitag-3090,vision-pulkitag-a6000,vision-pulkitag-v100
-#SBATCH -q vision-pulkitag-free-cycles
+#SBATCH -p vision-pulkitag-h100,vision-pulkitag-a6000,vision-pulkitag-3090,vision-pulkitag-v100
+#SBATCH -q vision-pulkitag-main
 #SBATCH --job-name=real_ol_vib
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64GB
-#SBATCH --time=0-16:00
+#SBATCH --time=0-09:50
 #SBATCH --gres=gpu:1
 
 # Run your command with the provided arguments
@@ -15,5 +15,6 @@ python -m src.train.bc +experiment=image/real_one_leg_insert furniture=one_leg_i
     training.ema.use=false training.ema.switch=false \
     vision_encoder=r3m vision_encoder.model=r3m_34 \
     vision_encoder.pretrained=true vision_encoder.freeze=false \
+    regularization.feature_layernorm=true \
     regularization.vib_front_feature_beta=0.1 \
     dryrun=false
