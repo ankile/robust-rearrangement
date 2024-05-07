68355e101fbae5e3ff6ee373a5530e16c77fb283
ankile <lars.ankile@gmail.com>
2024-05-06 20:05:46 -0400

    Fix partition issue for supercloud runs

--- slurm/bc/supercloud/one_leg_insert/real_one_leg_insert_obs_dropout.sh
+++ slurm/bc/supercloud/one_leg_insert/real_one_leg_insert_obs_dropout.sh
@@ -10,4 +10,7 @@ python -m src.train.bc +experiment=image/real_one_leg_insert \
     furniture=one_leg_insert \
     vision_encoder=r3m vision_encoder.model=r3m_18 \
     vision_encoder.pretrained=true vision_encoder.freeze=false \
+    regularization.front_camera_dropout=0.4 \
+    regularization.wrist_camera_dropout=0.4 \
+    regularization.proprioception_dropout=0.4 \
     dryrun=false
