dca859d7365248f19162c0b26968cf0f891fa02d
anthonysimeonov <asimeono@mit.edu>
2024-05-21 13:20:04 -0400

    better nominal camera params and poses for aligning robot in front/wrist image + matching realsense 435

--- src/sim2real/isaac_sim_raytrace.py
+++ src/sim2real/isaac_sim_raytrace.py
@@ -23,10 +23,21 @@ parser.add_argument("--headless",
 parser.add_argument('-i', '--demo-index',
                     type=int,
                     default=0)
+parser.add_argument('-sub', '--sub-steps',
+                    type=int,
+                    default=0)
 args_cli = parser.parse_args()
 
 # launch omniverse app
-simulation_app = SimulationApp({"headless": args_cli.headless})
+simulation_app = SimulationApp(
+    {
+        "headless": args_cli.headless,
+        # "renderer": "RayTracedLighting",
+        # "anti_aliasing": "FXAA",
+        "width": 640,
+        "height": 480
+    }
+)
 """Rest everything follows."""
 
 import torch
@@ -42,10 +53,11 @@ from omni.isaac.orbit.sensors.camera import Camera, PinholeCameraCfg
 import omni.replicator.core as rep
 from omni.isaac.orbit.utils import convert_dict_to_backend
 
-DEMO_DIR = "/data/scratch-oc40/pulkitag/ankile/furniture-data/raw/diffik/sim/one_leg/teleop/low/success"
+# DEMO_DIR = "/data/scratch-oc40/pulkitag/ankile/furniture-data/raw/diffik/sim/one_leg/teleop/low/success"
+DEMO_DIR = "/data/scratch-oc40/pulkitag/ankile/furniture-data/raw/diffik/sim/one_leg/teleop/med_perturb/success"
 FILES = [os.path.join(DEMO_DIR, f) for f in os.listdir(DEMO_DIR)]
-# FILE = FILES[args_cli.demo_index]
-FILE = "/data/scratch-oc40/pulkitag/ankile/furniture-data/raw/diffik/sim/one_leg/teleop/low/success/2024-05-09T15:32:54.pkl"
+FILE = FILES[args_cli.demo_index]
+# FILE = "/data/scratch-oc40/pulkitag/ankile/furniture-data/raw/diffik/sim/one_leg/teleop/low/success/2024-05-09T15:32:54.pkl"
 
 import numpy as np
 
@@ -58,10 +70,51 @@ april_to_sim_mat = np.array([[6.1232343e-17, 1.0000000e+00, 1.2246469e-16, 1.499
                              [0.0000000e+00, 1.2246469e-16, -1.0000000e+00, 4.1500002e-01],
                              [0.0000000e+00, 0.0000000e+00, 0.0000000e+00, 1.0000000e+00]])
 
+# april_to_sim_mat = np.array([[6.1232343e-17, 1.0000000e+00, 1.2246469e-16, 1.4999807e-03],
+#                              [1.0000000e+00, -6.1232343e-17, -7.4987988e-33, 0.0000000e+00],
+#                              [0.0000000e+00, 1.2246469e-16, -1.0000000e+00, 0.4],
+#                              [0.0000000e+00, 0.0000000e+00, 0.0000000e+00, 1.0000000e+00]])
+
+
+def run_until_quit(simulation_app, world):
+    import omni.appwindow
+    import carb.input
+
+    global RP_RUNNING, RP_PAUSE
+    RP_RUNNING = True
+    RP_PAUSE = False
+
+    def on_kb_input(e):
+        global RP_RUNNING, RP_PAUSE
+        if e.type == carb.input.KeyboardEventType.KEY_RELEASE:
+            if e.input == carb.input.KeyboardInput.Q:
+                print(f'[Run until quit] Caught "Q" keypress, closing app')
+                RP_RUNNING = False
+            elif e.input == carb.input.KeyboardInput.P:
+                print(f'[Run until quit] Caught "P" keypress, pausing')
+                RP_PAUSE = True
+
+    app_window = omni.appwindow.get_default_app_window()
+    kb = app_window.get_keyboard()
+    carb_input = carb.input.acquire_input_interface()
+    _ = carb_input.subscribe_to_keyboard_events(kb, on_kb_input)
+
+    while RP_RUNNING:
+        # simulation_app.update()
+        world.render()
+        time.sleep(1/120.0)
+
+        if RP_PAUSE:
+            print(f'[Run until quit] Paused!')
+            from IPython import embed; embed()
+            RP_PAUSE = False
+    
+    return
+
 
 def gen_prim(usd_path: str, prim_name: str, init_pos, init_ori):
-    # furniture_assets_root_path = Path(f"{os.getenv('RARL_SOURCE_DIR')}/sim2real/assets/furniture/mesh/usd")
-    furniture_assets_root_path = Path(f"assets/furniture/mesh/usd")
+    furniture_assets_root_path = Path(f"{os.getenv('RARL_SOURCE_DIR')}/sim2real/assets/furniture/mesh/usd")
+    # furniture_assets_root_path = Path(f"assets/furniture/mesh/usd")
     usd_path = str(furniture_assets_root_path / usd_path)
     pose = april_to_sim_mat @ (T.to_homogeneous(init_pos, init_ori))
     pos, ori = T.mat2pose(pose)
@@ -80,6 +133,11 @@ def main():
         data = pickle.load(f)
     """Spawns a single arm manipulator and applies random joint commands."""
 
+    # import carb
+    # carb.settings.get_settings().set("/omni/replicator/RTSubframes", 3)
+    # carb.settings.get_settings().set("/rtx/ambientOcclusion/enabled", False)
+    # rep.settings.set_render_rtx_realtime(antialiasing="FXAA")
+
     # Load kit helper
     # sim = SimulationContext(physics_dt=0.01, rendering_dt=0.01, backend="torch")
     sim = SimulationContext(physics_dt=0.0001, rendering_dt=0.0001, backend="torch")
@@ -88,11 +146,14 @@ def main():
     # Setup camera sensor
     camera_cfg = PinholeCameraCfg(
         sensor_tick=0,
-        height=720,
-        width=1280,
+        # height=720,
+        # width=1280,
+        height=480,
+        width=640,
         data_types=["rgb"],
         usd_params=PinholeCameraCfg.UsdCameraCfg(
-            focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
+            focal_length=18.8, horizontal_aperture=26.035, clipping_range=(0.1, 1.0e5)
+            # focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
         ),
     )
     camera = Camera(cfg=camera_cfg, device='cpu')
@@ -102,10 +163,10 @@ def main():
 
     # Create replicator writer
     demo_date = FILE.split('/')[-1].replace('.pkl', '')
-    output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output", "camera", f"{demo_date}_no_april")
+    output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output", "camera", f"{demo_date}_substeps_{args_cli.sub_steps}")
     rep_writer = rep.BasicWriter(output_dir=output_dir, frame_padding=3)
 
-    wrist_output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output", "wrist_camera", f"{demo_date}_no_april")
+    wrist_output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output", "wrist_camera", f"{demo_date}_substeps_{args_cli.sub_steps}")
     wrist_rep_writer = rep.BasicWriter(output_dir=wrist_output_dir, frame_padding=3)
 
     # Spawn things into stage
@@ -133,6 +194,30 @@ def main():
             "color": (1.0, 1.0, 1.0)
         },
     )
+
+    # Lights-2
+    # prim_utils.create_prim(
+    #     "/World/Light/WhiteSphere2",
+    #     "SphereLight",
+    #     translation=(0.5, -0.8, 5.0),
+    #     attributes={
+    #         "radius": 4.5,
+    #         "intensity": 1200.0,
+    #         "color": (1.0, 1.0, 1.0)
+    #     },
+    # )
+
+    # prim_utils.create_prim(
+    #     "/World/Light/WhiteSphere3",
+    #     "SphereLight",
+    #     translation=(0.5, 0.8, 5.0),
+    #     attributes={
+    #         "radius": 4.5,
+    #         "intensity": 1200.0,
+    #         "color": (1.0, 1.0, 1.0)
+    #     },
+    # )
+
     # Doom linght
     # prim_utils.create_prim(
     #     "/World/Light/DoomLight",
@@ -148,15 +233,21 @@ def main():
     # Table
     # table_usd_path = f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd"
 
-    # usd_base_path = Path(f"{os.getenv('RARL_SOURCE_DIR')}/sim2real/assets/furniture/mesh/usd")
-    usd_base_path = Path(f"assets/furniture/mesh/usd")
+    usd_base_path = Path(f"{os.getenv('RARL_SOURCE_DIR')}/sim2real/assets/furniture/mesh/usd")
+    # usd_base_path = Path(f"assets/furniture/mesh/usd")
     table_path = (usd_base_path / "table.usda")
     print(f'Table path: {table_path}')
+
     table_pos = (0., 0., 0.4)
+    # table_z_offset = 0.43
+    table_z_offset = 0.415
+
     prim_utils.create_prim(
         "/World/Table",
         usd_path=str(usd_base_path / "table.usda"))
         # translation=table_pos)
+    table_view = XFormPrimView("/World/Table", reset_xform_properties=False)
+    table_view.set_local_scales(torch.Tensor([[1.0, 1.75, 1.0]]))
 
     views = []
 
@@ -173,23 +264,27 @@ def main():
     #     usd_path=f'{os.path.dirname(os.path.abspath(__file__))}/../assets/furniture/mesh/base_tag.usd')
 
     # Obstacle front.
+    obs_quat_wxyz = np.array([0.7071067690849304, 0, 0, 0.7071067690849304])
     prim_utils.create_prim(
         '/World/ObstacleFront',
-        translation=(0.3815, 0., 0.43),
-        orientation=(0.7071067690849304, 0, 0, 0.7071067690849304),
+        translation=(0.3815, 0., table_z_offset),
+        orientation=obs_quat_wxyz,
         usd_path=str((usd_base_path / "obstacle_front.usda")))
 
     # Obstacle side.
     prim_utils.create_prim(
-        '/World/ObstacleSide1',
-        translation=(0.306, -0.175, 0.43),
-        orientation=(0.7071067690849304, 0, 0, 0.7071067690849304),
+        '/World/ObstacleRight',
+        translation=(0.306, -0.175, table_z_offset),
+        orientation=obs_quat_wxyz,
         usd_path=str((usd_base_path / "obstacle_side.usda")))
     prim_utils.create_prim(
-        '/World/ObstacleSide2',
-        translation=(0.306, 0.175, 0.43),
-        orientation=(0.7071067690849304, 0, 0, 0.7071067690849304),
+        '/World/ObstacleLeft',
+        translation=(0.306, 0.175, table_z_offset),
+        orientation=obs_quat_wxyz,
         usd_path=str((usd_base_path / "obstacle_side.usda")))
+    obstacle_center_view = XFormPrimView('/World/ObstacleFront', reset_xform_properties=False)
+    obstacle_right_view = XFormPrimView('/World/ObstacleRight', reset_xform_properties=False)
+    obstacle_left_view = XFormPrimView('/World/ObstacleLeft', reset_xform_properties=False)
 
     # bg_rot = rot_mat([0, 0, np.pi / 2])
     # bg_view = gen_prim('background.usd', , torch.tensor([0.8, 0, 0.75]), bg_rot)
@@ -227,7 +322,7 @@ def main():
 
     # -- Spawn robot
     robot = SingleArmManipulator(cfg=robot_cfg)
-    franka_from_origin_mat = np.array([[1., 0., 0., -0.3], [0., 1., 0., 0.], [0., 0., 1., 0.43],
+    franka_from_origin_mat = np.array([[1., 0., 0., -0.3], [0., 1., 0., 0.], [0., 0., 1., table_z_offset],
                                        [0., 0., 0., 1.]])
     pos, ori = T.mat2pose(franka_from_origin_mat)
     robot.spawn("/World/Robot_2", translation=pos)
@@ -239,15 +334,20 @@ def main():
         width=640,
         data_types=["rgb"],
         usd_params=PinholeCameraCfg.UsdCameraCfg(
-            focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
+            focal_length=18.8, horizontal_aperture=26.035, clipping_range=(0.001, 2.0)
+            # focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
         ),
     )
     wrist_camera = Camera(cfg=wrist_camera_cfg, device='cpu')
 
     # Spawn camera
     wrist_camera.spawn("/World/Robot_2/panda_hand",
-                       translation=np.array([0.1, 0.0, 0.04]),
-                       orientation=T.convert_quat(R.from_euler("XYZ", np.deg2rad([180, 45, -90])).as_quat(), to='wxyz'))
+                    #    translation=np.array([0.1, 0.0, 0.04]),
+                    #    translation=np.array([-0.05, 0.0, 0.0]),
+                       translation=np.array([-0.04, 0.0, 0.0655]),
+                    #    orientation=T.convert_quat(R.from_euler("XYZ", np.deg2rad([180, 45, -90])).as_quat(), to='wxyz'))
+                    #    orientation=T.convert_quat(R.from_euler("XYZ", np.deg2rad([180, -10, 90])).as_quat(), to='wxyz'))
+                       orientation=T.convert_quat(R.from_euler("XYZ", np.deg2rad([180, -17, 90])).as_quat(), to='wxyz'))
 
     # Play the simulator
     sim.reset()
@@ -260,9 +360,11 @@ def main():
     # Initialize camera
     camera.initialize()
 
-    cam_pos = (1.3, -0.00, 0.80)
-    cam_target = (-1, -0.00, 0.4)
-    camera.set_world_pose_from_view(cam_pos, cam_target)
+    # cam_pos = (0.775, -0.05, 0.85) # for objects/obstacles?
+    cam_pos = (0.61, -0.065, 0.82) # for robot?
+    # cam_quat = T.convert_quat(R.from_euler("XYZ", np.deg2rad([0.0, 65, 90])).as_quat(), to='wxyz') # for objects/obstacles?
+    cam_quat = T.convert_quat(R.from_euler("XYZ", np.deg2rad([0.0, 62, 90])).as_quat(), to='wxyz') # for robot?
+    camera._sensor_xform.set_world_pose(cam_pos, cam_quat)
 
     wrist_camera.initialize()
 
@@ -300,37 +402,49 @@ def main():
     parts_prev_goal_ori = []
     part_idx_offset = 1
 
-    prev_gripper_width = data['observations'][0]['robot_state']['gripper_width']
-    fj1 = data['observations'][0]['robot_state']['finger_joint_1']
-    fj2 = data['observations'][0]['robot_state']['finger_joint_2']
+    fj1 = data['observations'][0]['robot_state']['gripper_finger_1_pos']
+    fj2 = data['observations'][0]['robot_state']['gripper_finger_2_pos']
 
     for i in range(5):
         pos = data['observations'][part_idx_offset]['parts_poses'][7 * i:7 * i + 3]
         ori = data['observations'][part_idx_offset]['parts_poses'][7 * i + 3:7 * i + 7]
         parts_prev_goal_pos.append(pos)
         parts_prev_goal_ori.append(ori)
+    
+    obstacle_center_pose = data['observations'][1]['parts_poses'][-7:]
+    # obstacle_center_pose[2] += table_z_offset
+    obstacle_right_pose = obstacle_center_pose.copy() # ; obstacle_right_pose[0] -= 0.075; obstacle_right_pose[1] -= 0.175
+    obstacle_left_pose = obstacle_center_pose.copy() # ; obstacle_left_pose[0] -= 0.075; obstacle_left_pose[1] += 0.175
+
+    for obs_name, obs_view, obs_pose in zip(
+        ['center', 'right', 'left'],
+        [obstacle_center_view, obstacle_right_view, obstacle_left_view],
+        [obstacle_center_pose, obstacle_right_pose, obstacle_left_pose]):
+
+        rot = T.quat2mat(obs_pose[3:])
+        obs_pose_world = april_to_sim_mat @ (T.to_homogeneous(obs_pose[:3], rot))
+        obs_pose_pos, obs_pose_ori = T.mat2pose(obs_pose_world)
+        obs_pose_ori = T.convert_quat(obs_pose_ori, to='wxyz')
+
+        if obs_name == 'right':
+            obs_pose_pos[0] -= 0.075
+            obs_pose_pos[1] -= 0.175
+        elif obs_name == 'left':
+            obs_pose_pos[0] -= 0.075
+            obs_pose_pos[1] += 0.175
+
+        obs_pose_pos = torch.from_numpy(obs_pose_pos).unsqueeze(0)
+        obs_pose_ori = torch.from_numpy(obs_pose_ori).unsqueeze(0)
+
+        obs_view.set_world_poses(positions=obs_pose_pos, orientations=obs_pose_ori)
 
     for _ in range(100):
         sim.step()
         time.sleep(0.01)
-
-    from omni.isaac.core.utils.extensions import get_extension_path_from_name
-    from omni.isaac.motion_generation import ArticulationKinematicsSolver, LulaKinematicsSolver
-    from omni.isaac.motion_generation import interface_config_loader
-    mg_extension_path = get_extension_path_from_name("omni.isaac.motion_generation")
-    kinematics_config_dir = os.path.join(mg_extension_path, "motion_policy_configs")
-    kinematics_solver = LulaKinematicsSolver(
-        robot_description_path = kinematics_config_dir + "/franka/rmpflow/robot_descriptor.yaml",
-        urdf_path = kinematics_config_dir + "/franka/lula_franka_gen.urdf"
-    )
-    print("Valid frame names at which to compute kinematics:", kinematics_solver.get_all_frame_names())
-
-    body_names = robot.articulations.body_names
-
-    # links_view = XFormPrimView("/World/Robot_2/panda_link*", name="links_xform_prim_view")
-    # hand_view = XFormPrimView("/World/Robot_2/panda_hand", name="hand_xform_prim_view")
-    # right_finger_view = XFormPrimView("/World/Robot_2/panda_leftfinger*", name="left_finger_xform_prim_view")
-    # left_finger_view = XFormPrimView("/World/Robot_2/panda_rightfinger*", name="right_finger_xform_prim_view")
+    
+    # run_until_quit(simulation_app=simulation_app, world=sim) 
+    # from IPython import embed; embed()
+    # assert False
 
     fps_list = []
     last_time = time.time()
@@ -341,6 +455,10 @@ def main():
 
         part_idx = obs_idx + part_idx_offset if obs_idx + part_idx_offset < len(
             data['observations']) else len(data['observations']) - 1
+
+        # if obs_idx == 50:
+        #     run_until_quit(simulation_app=simulation_app, world=sim)
+        #     assert False
 
         for i in range(int(sim_steps)):
             interp_goal = prev_goal_pos + (i + 1) * dx
@@ -364,14 +482,9 @@ def main():
                 print(f'Frames per second: {fps_avg}')
             last_time = time.time()
 
-            griper_dx = (obs['robot_state']['gripper_width'] - prev_gripper_width) / sim_steps
-            gripper_interp_goal = prev_gripper_width + (i + 1) * griper_dx
-
             robot.set_dof_state(
                 torch.concat([
                     interp_goal,
-                    # torch.from_numpy(np.array([gripper_interp_goal])).float() / 2,
-                    # torch.from_numpy(np.array([gripper_interp_goal])).float() / 2
                     torch.from_numpy(fj1.reshape(1,)).float(),
                     torch.from_numpy(fj2.reshape(1,)).float()
                 ]),
@@ -380,20 +493,6 @@ def main():
                     torch.zeros((1, )),
                     torch.zeros((1, ))
                 ]))
-
-            robot_world_poses = []
-            for ii, body_name in enumerate(robot.articulations.body_names):
-                if 'finger' in body_name:
-                    continue
-                body_position, rot_mat = kinematics_solver.compute_forward_kinematics(body_name, data['observations'][i]['robot_state']['joint_positions'])
-                body_quat_wxyz = T.convert_quat(R.from_matrix(rot_mat).as_quat())
-                body_pose_arr = np.concatenate([body_position, body_quat_wxyz])
-                robot_world_poses.append(torch.from_numpy(body_pose_arr).float())
-            link_world_poses = torch.stack(robot_world_poses[:-1])
-            hand_world_pose = robot_world_poses[-1]
-
-            # this function call below doesn't work (non-root links, already wrapped in the articulation view)
-            # links_view.set_world_poses(positions=link_world_poses[:, :3], orientations=link_world_poses[:, 3:7])
 
             for j in range(5):
                 # pos = obs['parts_poses'][7 * i:7 * i + 3]
@@ -415,7 +514,9 @@ def main():
 
             # perform step
             sim.step()
-            # sim.render()
+            # rep.orchestrator.step(rt_subframes=8)
+            for _ in range(args_cli.sub_steps):
+                sim.render()
             
             # update sim-time
             sim_time += sim_dt
@@ -427,9 +528,8 @@ def main():
 
 
         prev_goal_pos = torch.tensor(obs['robot_state']['joint_positions'])
-        prev_gripper_width = obs['robot_state']['gripper_width']
-        fj1 = obs['robot_state']['finger_joint_1']
-        fj2 = obs['robot_state']['finger_joint_2']
+        fj1 = obs['robot_state']['gripper_finger_1_pos']
+        fj2 = obs['robot_state']['gripper_finger_2_pos']
         parts_prev_goal_pos = []
         parts_prev_goal_ori = []
         for i in range(5):
