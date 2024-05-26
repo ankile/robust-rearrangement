28cb570248a062a6ff9b2d4a2cca6f3ae7dca36b
ankile <lars.ankile@gmail.com>
2024-05-26 17:12:23 -0400

    Add automatic caching of the base policy files in residual ppo

--- slurm/bc/supercloud/diffusion/1_diff_unet_sm.sh
+++ slurm/bc/supercloud/diffusion/1_diff_unet_sm.sh
@@ -7,6 +7,6 @@
 #SBATCH -c 20
 
 python -m src.train.bc +experiment=state/diff_unet \
-    actor.diffusion_model.down_dims='[64,128, 256]' \
+    actor.diffusion_model.down_dims='[64,128,256]' \
     wandb.mode=offline \
     dryrun=false
