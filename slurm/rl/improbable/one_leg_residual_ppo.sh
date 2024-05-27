fa3c4a8509f98ed8e440ba7700cba5f7f6869ab3
ankile <lars.ankile@gmail.com>
2024-05-27 14:27:01 -0400

    Start a proper pipeline debug run

--- slurm/rl/improbable/one_leg_residual_ppo.sh
+++ slurm/rl/improbable/one_leg_residual_ppo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p vision-pulkitag-3090,vision-pulkitag-a6000,vision-pulkitag-v100,vision-pulkitag-a100
-#SBATCH -q vision-pulkitag-main
+#SBATCH -q vision-pulkitag-free-cycles
 #SBATCH --job-name=ol_res_ppo_new
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
