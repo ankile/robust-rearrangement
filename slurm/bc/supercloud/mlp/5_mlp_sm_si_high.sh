76c4c34c36ca0c3ef4f8b2e3f3e1517c6a408c5c
ankile <lars.ankile@gmail.com>
2024-05-28 15:26:02 -0400

    Add supercloud high mlp experiments

---
+++ slurm/bc/supercloud/mlp/5_mlp_sm_si_high.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p xeon-g6-volta
+#SBATCH -t 0-12:00
+#SBATCH --gres=gpu:volta:1
+#SBATCH -c 20
+#SBATCH --job-name=5_mlp_sm_si_high
+
+python -m src.train.bc +experiment=state/mlp_sm_si \
+    randomness='[high]' \
+    data.data_subset=50 \
+    rollout.randomness=high \
+    wandb.project=ol-state-dr-high-1 \
+    wandb.mode=offline \
+    dryrun=false
---
+++ slurm/bc/supercloud/mlp/8_mlp_lg_ch_high.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p xeon-g6-volta
+#SBATCH -t 0-12:00
+#SBATCH --gres=gpu:volta:1
+#SBATCH -c 20
+#SBATCH --job-name=8_mlp_lg_ch_high
+
+python -m src.train.bc +experiment=state/mlp_lg_ch \
+    randomness='[high]' \
+    data.data_subset=50 \
+    rollout.randomness=high \
+    wandb.project=ol-state-dr-high-1 \
+    wandb.mode=offline \
+    dryrun=false
