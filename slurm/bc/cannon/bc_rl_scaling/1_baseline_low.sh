28f5d377c706cd0e2afd76ce922ce5581f4a1218
ankile <lars.ankile@gmail.com>
2024-06-01 12:33:06 -0400

    Low scaling runs for cannon

--- slurm/bc/cannon/bc_rl_scaling/1_baseline_low.sh
+++ slurm/bc/cannon/bc_rl_scaling/1_baseline_low.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #SBATCH -p gpu
-#SBATCH -t 3-00:00
+#SBATCH -t 2-00:00
 #SBATCH --mem=256G
 #SBATCH --gres=gpu:1
 #SBATCH -c 16
 #SBATCH --account=parkes_low_priority
-#SBATCH --job-name=1_diff_bc_rollouts
+#SBATCH --job-name=1_baseline_low
 
 # Run vision-based training on one_leg furniture with low randomness
 # with only the original 50 teleop demos as a baseline
 
 python -m src.train.bc +experiment=image/diff_unet \
     rollout=rollout \
+    rollout.num_envs=128 \
     demo_source=teleop \
     furniture=one_leg \
     randomness='[low,low_perturb]' \
