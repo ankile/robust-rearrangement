6d03e7981a6c16e3dc580e59ea15aa6c589cd86d
ankile <lars.ankile@gmail.com>
2024-06-02 01:15:56 -0400

    Increase the wandb-osh sync interval

--- slurm/bc/improbable/lamp/diff/2_unet_lg_low.sh
+++ slurm/bc/improbable/lamp/diff/2_unet_lg_low.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -p vision-pulkitag-a100,vision-pulkitag-a6000,vision-pulkitag-3090
+#SBATCH -p vision-pulkitag-a6000,vision-pulkitag-3090,vision-pulkitag-a100,vision-pulkitag-v100
 #SBATCH -q vision-pulkitag-free-cycles
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
