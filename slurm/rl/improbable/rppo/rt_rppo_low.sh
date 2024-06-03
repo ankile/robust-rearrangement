e487a2fcb71ee8a1fde36c7c1441e7e28860a6db
ankile <lars.ankile@gmail.com>
2024-06-03 14:47:28 -0400

    Restart lamp rl run

--- slurm/rl/improbable/rppo/rt_rppo_low.sh
+++ slurm/rl/improbable/rppo/rt_rppo_low.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p vision-pulkitag-3090,vision-pulkitag-a6000,vision-pulkitag-a100,vision-pulkitag-v100
-#SBATCH -q vision-pulkitag-free-cycles
+#SBATCH -q vision-pulkitag-main
 #SBATCH --job-name=rt_rppo_low
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
