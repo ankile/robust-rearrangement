3e2397ca7c59e65e373a40e1a07876e2e0af61fa
ankile <lars.ankile@gmail.com>
2024-06-05 01:41:58 -0400

    Add cotraining for black parts

--- slurm/bc/improbable/bc_rl_scaling/low/8_rl_100050_low_state.sh
+++ slurm/bc/improbable/bc_rl_scaling/low/8_rl_100050_low_state.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem=64GB
 #SBATCH --time=2-00:00
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=7_rl_50050_low_state
+#SBATCH --job-name=8_rl_100050_low_state
 
-python -m src.train.bc +experiment=state/scaling_50k \
+python -m src.train.bc +experiment=state/scaling_100k \
     observation_type=state \
     dryrun=false
