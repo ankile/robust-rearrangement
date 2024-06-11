60e86badb0f578f5fc1c26e1b71490f164f4533f
ankile <lars.ankile@gmail.com>
2024-06-10 23:39:36 -0400

    Add more history runs

--- slurm/bc/improbable/real_lp_fewshot_cotrain/40/4a_lp_cotrain_40_hist2.sh
+++ slurm/bc/improbable/real_lp_fewshot_cotrain/40/4a_lp_cotrain_40_hist2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p vision-pulkitag-h100,vision-pulkitag-a100,vision-pulkitag-a6000,vision-pulkitag-3090
-#SBATCH -q vision-pulkitag-main
+#SBATCH -q vision-pulkitag-free-cycles
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
