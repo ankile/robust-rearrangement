0efc0d5dc0af8ef11be187cbb4f94956a8375105
ankile <lars.ankile@gmail.com>
2024-06-10 23:59:34 -0400

    Increase memory allowance for lamp cotrain improbable jobs

--- slurm/bc/improbable/real_lp_fewshot_cotrain/40/3_lp_cotrain_40_upsamp.sh
+++ slurm/bc/improbable/real_lp_fewshot_cotrain/40/3_lp_cotrain_40_upsamp.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
 #SBATCH --time=2-00:00
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=1_lp_10
