9cb5bcf21425e947dd1ba5f7e6dd83e4e149c620
ankile <lars.ankile@gmail.com>
2024-07-29 18:48:54 -0400

    Add Dinov2 and CLIP ViT Vision encoders

--- src/models/vision.py
+++ src/models/vision.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import timm.data
 import torch
 import torch.nn as nn
 import torchvision
@@ -134,20 +135,20 @@ class ResnetEncoder(VisionEncoder):
 
 
 class DinoV2Encoder(torch.nn.Module):
-    def __init__(self, model_name="dinov2-base", freeze=True, device="cuda"):
+    def __init__(self, model_name="dinov2_vits14", freeze=True, device="cuda"):
         super().__init__()
         assert model_name in [
-            "dinov2-small",
-            "dinov2-base",
-            "dinov2-large",
-            "dinov2-giant",
+            "dinov2_vits14",
+            "dinov2_vitb14",
+            "dinov2_vitl14",
+            "dinov2_vitg14",
         ]
         self.device = device
 
-        model_name = f"facebook/{model_name}"
-        self.trans = transformers.AutoImageProcessor.from_pretrained(model_name)
-        self.model = transformers.AutoModel.from_pretrained(model_name).to(self.device)
-        self.encoding_dim = self.model.config.hidden_size
+        # Model wants a batch of images of shape (batch_size, 3, 224, 224) and normalized
+        self.model = torch.hub.load("facebookresearch/dinov2", model_name)
+
+        self.encoding_dim = self.model.norm.normalized_shape[0]
 
         if freeze:
             for param in self.model.parameters():
@@ -155,10 +156,56 @@ class DinoV2Encoder(torch.nn.Module):
 
             self.model.eval()
 
+        self.model = self.model.to(device)
+
+        self.normalize = torchvision.transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        )
+
     # Expect input to be a batch of images of shape (batch_size, 3, 224, 224) in range [0, 255]
     def forward(self, x):
-        x = self.trans(x, return_tensors="pt").pixel_values.to(self.device)
-        x = self.model(x).pooler_output
+        # Normalize images
+        x = x / 255.0
+        x = self.normalize(x)
+        x = self.model(x)
+        return x
+
+
+class ClipViTEncoder(torch.nn.Module):
+    def __init__(self, model_name="vit_clip", freeze=True, device="cuda"):
+        super().__init__()
+        self.device = device
+
+        import timm
+        from timm.models.vision_transformer import VisionTransformer
+
+        # Model wants a batch of images of shape (batch_size, 3, 224, 224) and normalized
+        self.model: VisionTransformer = timm.create_model(
+            model_name="vit_base_patch16_clip_224.openai",
+            pretrained=True,
+            global_pool="token",
+            num_classes=0,
+        )
+
+        self.encoding_dim = self.model.embed_dim
+
+        if freeze:
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+            self.model.eval()
+
+        self.model = self.model.to(device)
+
+        data_cfg = timm.data.resolve_data_config(self.model.pretrained_cfg)
+        self.normalize = timm.data.create_transform(**data_cfg).transforms[-1]
+
+    # Expect input to be a batch of images of shape (batch_size, 3, 224, 224) in range [0, 255]
+    def forward(self, x):
+        # Normalize images
+        x = x / 255.0
+        x = self.normalize(x)
+        x = self.model(x)
         return x
 
 
