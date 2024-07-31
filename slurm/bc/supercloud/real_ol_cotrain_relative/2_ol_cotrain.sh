b49647505c7f97ee8d72fee0cbf512588f7b8dde
ankile <lars.ankile@gmail.com>
2024-07-31 19:34:05 -0400

    Update the supercloud relative action scripts

--- slurm/bc/supercloud/real_ol_cotrain_relative/2_ol_cotrain.sh
+++ slurm/bc/supercloud/real_ol_cotrain_relative/2_ol_cotrain.sh
@@ -2,18 +2,18 @@
 
 #SBATCH -p xeon-g6-volta
 #SBATCH -t 3-00:00
-#SBATCH --gres=gpu:volta:4
-#SBATCH -c 80
+#SBATCH --gres=gpu:volta:2
+#SBATCH -c 40
 #SBATCH --job-name=1_ol_50_gpu
 
 OMP_NUM_THREADS=20
 
-python -m src.train.bc +experiment=image/real_ol_cotrain \
+torchrun --standalone --nproc_per_node=2 -m src.train.bc_ddp +experiment=image/real_ol_cotrain \
     demo_source='[teleop,rollout]' \
-    furniture='[one_leg_render_rppo,one_leg_full,one_leg_highres]' \
+    furniture='[one_leg_full,one_leg_render_rppo_brighter,one_leg_render_demos_brighter]' \
     randomness='[low,med,med_perturb]' \
     environment='[real,sim]' \
-    training.batch_size=64 \
+    training.batch_size=128 \
     data.dataloader_workers=20 \
     control.control_mode=relative \
     training.clip_grad_norm=true \
