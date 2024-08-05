0b9f979ab12a0ed0e0a5a8b4f265be846218de31
ankile <lars.ankile@gmail.com>
2024-08-05 10:26:34 -0400

    Run ablation experiments on demo number and residual action scale

--- slurm/bc/improbable/bc_rl_scaling/low/7_rl_50050_low_state.sh
+++ slurm/bc/improbable/bc_rl_scaling/low/7_rl_50050_low_state.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH -p vision-pulkitag-a6000,vision-pulkitag-v100,vision-pulkitag-a100,vision-pulkitag-3090
-#SBATCH -q vision-pulkitag-main
+#SBATCH -p vision-pulkitag-a100,vision-pulkitag-a6000,vision-pulkitag-v100,vision-pulkitag-3090
+#SBATCH -q vision-pulkitag-free-cycles
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
@@ -9,6 +9,8 @@
 #SBATCH --time=2-00:00
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=7_rl_50050_low_state
+#SBATCH --requeue
 
 python -m src.train.bc +experiment=state/scaling_50k \
+    wandb.continue_run_id=shc03kek \
     dryrun=false
--- slurm/bc/improbable/bc_rl_scaling/low/8_rl_100050_low_state.sh
+++ slurm/bc/improbable/bc_rl_scaling/low/8_rl_100050_low_state.sh
@@ -6,10 +6,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128GB
-#SBATCH --time=3-00:00
+#SBATCH --time=1-00:00
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=8_rl_100050_low_state
 
 python -m src.train.bc +experiment=state/scaling_100k \
-    training.gpu_id=0 \
-    dryrun=true
+    dryrun=false
