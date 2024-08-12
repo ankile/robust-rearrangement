7b65ae435a0d5a26d5cec2be4d275b465dcd3d42
anthonysimeonov <asimeono@mit.edu>
2024-08-12 11:17:35 -0400

    update hyperparams for MLP PPO exp and add fph mlp BC run

--- slurm/rl/improbable/mppo/fph_mlp_ppo_low.sh
+++ slurm/rl/improbable/mppo/fph_mlp_ppo_low.sh
@@ -8,19 +8,19 @@
 #SBATCH --mem=64GB
 #SBATCH --time=2-00:00
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=mr_mppo_low
+#SBATCH --job-name=fph_mppo_low
 
 python -m src.train.ppo \
     base_policy.wandb_id=mr-state-dr-low-1/6zwxy8h8 \
     base_policy.wt_type=best_success_rate \
-    env.task=mug_rack \
+    env.task=factory_peg_hole \
     env.randomness=low \
-    num_env_steps=400 \
+    num_env_steps=200 \
     num_envs=1024 \
     kl_coef=0.5 \
     gae_lambda=1.0 \
     gamma=1.0 \
     init_logstd=-4.0 \
     vf_coef=1.0 \
-    wandb.project=mr-mlp-ppo-dr-low-1 \
+    wandb.project=fph-mlp-ppo-dr-low-1 \
     debug=false
