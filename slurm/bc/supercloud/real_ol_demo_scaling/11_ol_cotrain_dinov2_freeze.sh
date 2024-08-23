618a22b4dcb09a9a2daef471da118874f7e03ff3
ankile <lars.ankile@gmail.com>
2024-08-22 21:59:19 -0400

    Add frozen encoders for co-training for curiosity

--- slurm/bc/supercloud/real_ol_demo_scaling/11_ol_cotrain_dinov2_freeze.sh
+++ slurm/bc/supercloud/real_ol_demo_scaling/11_ol_cotrain_dinov2_freeze.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 1-12:00
 #SBATCH --gres=gpu:volta:2
 #SBATCH -c 40
-#SBATCH --job-name=7_ol_cotrain
+#SBATCH --job-name=11_ol_cotrain_dinov2_freeze
 
 export OMP_NUM_THREADS=20
 
@@ -14,10 +14,10 @@ torchrun --standalone --nproc_per_node=2 -m src.train.bc_ddp +experiment=image/r
     randomness='[low,med,med_perturb]' \
     training.clip_grad_norm=true \
     training.batch_size=128 \
+    vision_encoder=dinov2 \
+    vision_encoder.freeze=true \
     data.dataloader_workers=20 \
     environment='[real,sim]' \
-    data.minority_class_power=3 \
-    actor.confusion_loss_beta=1e-4 \
     wandb.mode=offline \
     wandb.project=real-ol-demo-scaling-1 \
     wandb.name=ol-40-cotrain-1 \
