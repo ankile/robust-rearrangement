9436f14a78445df137b85bd9f9560bfccd4a7496
anthonysimeonov <asimeono@mit.edu>
2024-08-26 16:37:57 -0400

    roll back to previous transforms version that works with older pytorch (for real world)

--- src/common/vision.py
+++ src/common/vision.py
@@ -1,92 +1,82 @@
 import torch
 import torch.nn as nn
+import torchvision
+
+# torchvision.disable_beta_transforms_warning()
+
+from torchvision import transforms
 
 # from torchvision.transforms import v2 as transforms
-
-from torchvision.transforms import transforms
-
 from ipdb import set_trace as bp  # noqa
 
 
-class TransformWrapper(torch.nn.Module):
-    def __init__(self, transform):
-        super().__init__()
-        self.transform = transform
-
-    def forward(self, x):
-        return self.transform(x)
-
-
 class FrontCameraTransform(nn.Module):
-
     def __init__(self, mode="train"):
         super().__init__()
+        self.mode = mode
+
         margin = 20
         crop_size = (224, 224)
         input_size = (240, 320)
 
-        self.transform_train = None
-        # self.transform_train = transforms.Compose(
-        #     [
-        #         transforms.ColorJitter(
-        #             brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3
-        #         ),
-        #         transforms.GaussianBlur(kernel_size=5, sigma=(0.01, 1.2)),
-        #         transforms.CenterCrop((input_size[0], input_size[1] - 2 * margin)),
-        #         transforms.RandomCrop(crop_size),
-        #         transforms.RandomErasing(value="random", p=0.2),
-        #     ]
-        # )
+        self.transform_train = transforms.Compose(
+            [
+                transforms.ColorJitter(
+                    brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3
+                ),
+                transforms.GaussianBlur(kernel_size=5, sigma=(0.01, 2.0)),
+                transforms.CenterCrop((input_size[0], input_size[1] - 2 * margin)),
+                transforms.RandomCrop(crop_size),
+            ]
+        )
         self.transform_eval = transforms.CenterCrop(crop_size)
 
-        self.transform = (
-            self.transform_train if mode == "train" else self.transform_eval
-        )
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        assert x.shape[-3:] == (3, 240, 320), x.shape
-        return self.transform(x)
+        assert x.shape[2:] == (240, 320), f"Invalid input shape: {x.shape}"
+        if self.mode == "train":
+            return self.transform_train(x)
+        elif self.mode == "eval":
+            return self.transform_eval(x)
+
+        raise ValueError(f"Invalid mode: {self.mode}")
 
     def train(self, mode=True):
         super().train(mode)
-        # self.transform = self.transform_train if mode else self.transform_eval
+        self.mode = "train" if mode else "eval"
 
     def eval(self):
         super().eval()
-        self.transform = self.transform_eval
+        self.mode = "eval"
 
 
 class WristCameraTransform(nn.Module):
-    transform: nn.Module
-
     def __init__(self, mode="train"):
         super().__init__()
+        self.mode = mode
 
-        self.transform_train = None
-        # self.transform_train = transforms.Compose(
-        #     [
-        #         transforms.ColorJitter(
-        #             brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3
-        #         ),
-        #         transforms.GaussianBlur(kernel_size=5, sigma=(0.01, 1.2)),
-        #         transforms.Resize((224, 224), antialias=True),
-        #     ]
-        # )
+        self.transform_train = transforms.Compose(
+            [
+                transforms.ColorJitter(
+                    brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3
+                ),
+                transforms.GaussianBlur(kernel_size=5, sigma=(0.01, 2.0)),
+                transforms.Resize((224, 224), antialias=True),
+            ]
+        )
         self.transform_eval = transforms.Resize((224, 224), antialias=True)
 
-        self.transform = (
-            self.transform_train if mode == "train" else self.transform_eval
-        )
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        assert x.shape[-3:] == (3, 240, 320), x.shape
+        if self.mode == "train":
+            return self.transform_train(x)
+        elif self.mode == "eval":
+            return self.transform_eval(x)
 
-        return self.transform(x)
+        raise ValueError(f"Invalid mode: {self.mode}")
 
     def train(self, mode=True):
         super().train(mode)
-        # self.transform = self.transform_train if mode else self.transform_eval
+        self.mode = "train" if mode else "eval"
 
     def eval(self):
         super().eval()
-        self.transform = self.transform_eval
+        self.mode = "eval"
