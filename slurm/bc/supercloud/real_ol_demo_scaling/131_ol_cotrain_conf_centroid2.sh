5d67673a454aa79538a6d2ec523e5da0bb324dec
ankile <lars.ankile@gmail.com>
2024-09-08 13:32:10 -0400

    Analyze confusion with centroid formulation

--- slurm/bc/supercloud/real_ol_demo_scaling/131_ol_cotrain_conf_centroid2.sh
+++ slurm/bc/supercloud/real_ol_demo_scaling/131_ol_cotrain_conf_centroid2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH -p xeon-g6-volta
-#SBATCH -t 1-12:00
+#SBATCH -t 1-00:00
 #SBATCH --gres=gpu:volta:2
 #SBATCH -c 40
-#SBATCH --job-name=13_ol_cotrain_conf_centroid3
+#SBATCH --job-name=131_ol_cotrain_conf_centroid2
 
 export OMP_NUM_THREADS=20
 
@@ -17,9 +17,9 @@ torchrun --standalone --nproc_per_node=2 -m src.train.bc_ddp +experiment=image/r
     data.dataloader_workers=20 \
     environment='[real,sim]' \
     data.minority_class_power=3 \
-    actor.confusion_loss_beta=1e-3 \
+    actor.confusion_loss_beta=1e-2 \
     actor.confusion_loss_centroid_formulation=true \
     wandb.mode=offline \
     wandb.project=real-ol-demo-scaling-1 \
-    wandb.name=ol-40-cotrain-cf-centroid-e3-1 \
+    wandb.name=ol-40-cotrain-cf-centroid-e2-1 \
     dryrun=false
